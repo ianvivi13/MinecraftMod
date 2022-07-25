0f853b2a3fd223e212859c43a16adc0781029def
UnicycleUnicorn <brand227796@gmail.com>
2022-07-24 20:39:42 -0400

    Advanced chat features
    ======== STYLING ========
    * %....% to style
    * %% will place a % in text (escape sequence)
    * multiple styles can be placed the in same tag w/ or w/o spaces
    
    -b: bold
    -s: strikethrough
    -i: italics
    -u: underline
    -g: obfuscated
    ** a "/" can be appended to the above to turn that styling off **
    -r: reset
    -#000000: color
    
    ======== Special ========
    * $....$ to add a special character
    * $$ will place a $ in text (escape sequence)
    
    -keyword (like "pi" or "1/2")
    -#000000: hex unicode character (emojis not supported by minecraft)
    
    ========== URL ==========
    * urls automatically styled as blue, clickable, hover-able, italicized
    
    -url(link,name) will add a clickable and hover-able link to chat (with "name" as the text)
    -url(link) will add a clickable and hover-able link to chat (wiht "link" as the text)

--- src/main/java/com/bic/bit_o_everything/ModUtils.java
+++ src/main/java/com/bic/bit_o_everything/ModUtils.java
@@ -1,10 +1,21 @@
 package com.bic.bit_o_everything;
 
+import com.ibm.icu.impl.Pair;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.*;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.HexFormat;
+import static java.util.Map.entry;
+
+import java.util.List;
+import java.util.Map;
 
 public final class ModUtils {
 
@@ -91,4 +102,159 @@ public final class ModUtils {
 
     }
 
+    public static class advancedChat {
+        private enum sectionType {
+            REGULAR_TEXT,
+            URL,
+            STYLE_MODIFIER
+        }
+
+        private static final Map<String, String> chatMap = Map.ofEntries(
+                entry(" ", "$"),
+                entry("pi", "π"),
+                entry("copy", "©"),
+                entry("1/4","¼"),
+                entry("1/2","½"),
+                entry("3/4","¾"),
+                entry("degree","°")
+        );
+
+        public static Component format(String message) {
+            message = replaceSymbols(message);
+            return buildFancyComponent(separateSections(message));
+        }
+
+        private static Style editStyle(Style currentStyle, String s) {
+            StringBuilder col = new StringBuilder("=======");
+            char[] arr = s.toCharArray();
+            boolean currentBool = true;
+            for (char item: arr) {
+                if (col.toString().length() <= 6) {
+                    col.append(item);
+                    if (col.toString().length() == 7) {
+                        currentStyle = currentStyle.withColor(TextColor.parseColor(col.toString()));
+                    }
+                } else {
+                    switch (item) {
+                        case 'b' -> currentStyle = currentStyle.withBold(currentBool);
+                        case 'i' -> currentStyle = currentStyle.withItalic(currentBool);
+                        case 'g' -> currentStyle = currentStyle.withObfuscated(currentBool);
+                        case 'u' -> currentStyle = currentStyle.withUnderlined(currentBool);
+                        case 's' -> currentStyle = currentStyle.withStrikethrough(currentBool);
+                        case 'r' -> currentStyle = Style.EMPTY;
+                        case '#' -> col = new StringBuilder("#");
+                    }
+                    currentBool = item != '/';
+                }
+            }
+            return currentStyle;
+        }
+
+        private static List<Pair<String, sectionType>> separateUrls(String message) {
+            List<Pair<String, sectionType>> ret = new ArrayList<>();
+            if (message.contains("url(")) {
+                String[] parts = message.split("url\\(",2);
+                String beforeUrl = parts[0];
+                String url = StringUtils.substringBefore(parts[1],")");
+                String afterUrl = StringUtils.substringAfter(parts[1],")");
+                ret.add(Pair.of(beforeUrl, sectionType.REGULAR_TEXT));
+                ret.add(Pair.of(url, sectionType.URL));
+                if (!afterUrl.equals("")) {
+                    ret.addAll(separateUrls(afterUrl));
+                }
+            } else {
+                ret.add(Pair.of(message, sectionType.REGULAR_TEXT));
+            }
+            return ret;
+        }
+
+        private static List<Pair<String, sectionType>> separateModifiers(String message) {
+            List<Pair<String, sectionType>> ret = new ArrayList<>();
+
+            boolean insideExpression = false;
+            message = message.replace("%%","% %");
+            String[] arr = message.split("%");
+            for (String item: arr) {
+                if (insideExpression) {
+                    if (item.equals(" ")) {
+                        ret.add(Pair.of("|",sectionType.REGULAR_TEXT));
+                    } else {
+                        ret.add(Pair.of(item.replace(" ",""),sectionType.STYLE_MODIFIER));
+                    }
+                } else {
+                    if (!item.equals("")) {
+                        ret.add(Pair.of(item, sectionType.REGULAR_TEXT));
+                    }
+                }
+                insideExpression = !insideExpression;
+            }
+
+            return ret;
+        }
+
+        private static List<Pair<String, sectionType>> separateSections(String message) {
+            List<Pair<String, sectionType>> ret = new ArrayList<>();
+            List<Pair<String, sectionType>> urlSeparated = separateUrls(message);
+            for (Pair<String, sectionType> item: urlSeparated) {
+                if (item.second.equals(sectionType.URL)) {
+                    ret.add(item);
+                } else {
+                    ret.addAll(separateModifiers(item.first));
+                }
+            }
+
+            return ret;
+        }
+
+        private static Component buildFancyComponent(List<Pair<String, sectionType>> messageParts) {
+            MutableComponent newMessage = MutableComponent.create(ComponentContents.EMPTY);
+            Style currentStyle = Style.EMPTY;
+
+            for (Pair<String, sectionType> item: messageParts) {
+                switch (item.second) {
+                    case URL -> {
+                        String url = item.first;
+                        String text = item.first;
+                        if (item.first.contains(",")) {
+                            String[] brokenUrl = item.first.split(",");
+                            url = brokenUrl[0];
+                            text = brokenUrl[1];
+                        }
+                        ClickEvent urlEvent = new ClickEvent(ClickEvent.Action.OPEN_URL, url);
+                        HoverEvent hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal(url));
+                        newMessage.append(Component.literal(text).withStyle(Style.EMPTY.withClickEvent(urlEvent).withItalic(true).withColor(ChatFormatting.BLUE).withUnderlined(true).withHoverEvent(hoverEvent)));
+                    }
+                    case STYLE_MODIFIER -> currentStyle = editStyle(currentStyle, item.first);
+                    case REGULAR_TEXT -> newMessage.append(Component.literal(item.first).withStyle(currentStyle));
+                }
+            }
+            return newMessage;
+        }
+
+        private static String replaceSymbols(String string) {
+            StringBuilder ret = new StringBuilder();
+            string = string.replace("$$","$ $");
+            String[] arr = string.split("\\$");
+            boolean inExpression = false;
+            for (String item: arr) {
+                if (inExpression) {
+                    if (item.startsWith("#")) {
+                        try {
+                            int i = HexFormat.fromHexDigits(item.substring(1));
+                            ret.appendCodePoint(i);
+                        } catch (Exception ignored) {}
+                    } else {
+                        if (advancedChat.chatMap.get(item.toLowerCase()) != null) {
+                            ret.append(advancedChat.chatMap.get(item.toLowerCase()));
+                        }
+                    }
+                } else {
+                    ret.append(item);
+                }
+                inExpression = !inExpression;
+            }
+            return ret.toString();
+        }
+
+    }
 }
--- src/main/java/com/bic/bit_o_everything/events/ModEvents.java
+++ src/main/java/com/bic/bit_o_everything/events/ModEvents.java
@@ -1,14 +1,24 @@
 package com.bic.bit_o_everything.events;
 
 import com.bic.bit_o_everything.BitOEverything;
+import com.bic.bit_o_everything.ModUtils;
 import com.bic.bit_o_everything.item.custom.fancyTypes.EmptyLeftClick;
 import com.bic.bit_o_everything.item.custom.fancyTypes.LeftClickBlock;
 import com.bic.bit_o_everything.network.ModPacketHandler;
 import com.bic.bit_o_everything.network.ServerboundEmptyLeftClickPacket;
+import io.netty.handler.codec.string.StringEncoder;
+import net.minecraft.Util;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.commons.lang3.StringEscapeUtils;
+
+import java.util.HexFormat;
+import java.util.List;
+import java.util.Map;
 
 @Mod.EventBusSubscriber(modid = BitOEverything.MOD_ID)
 public class ModEvents {
@@ -27,6 +37,14 @@ public class ModEvents {
         ItemStack itemStack =  event.getItemStack();
         if (itemStack.getItem() instanceof LeftClickBlock leftClickBlock) {
             leftClickBlock.leftClickBlock(event.getEntity(), itemStack);
+        }
+    }
+
+    @SubscribeEvent
+    public static void clientReceivedChat(ClientChatReceivedEvent event) {
+        if (!event.getChatSender().uuid().equals(Util.NIL_UUID)) {
+            String message = event.getMessage().getString();
+            event.setMessage(ModUtils.advancedChat.format(message));
         }
     }
 }
